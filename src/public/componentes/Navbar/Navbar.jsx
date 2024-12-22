1153199c7d1e56201a93d2430c3fd39e4bc18760
Gabriel Bunhak <bunhakgabriel@gmail.com>
2024-12-22 17:00:09 -0300

    feat: add estruturação de pastas do projeto

---
+++ src/public/componentes/Navbar/Navbar.jsx
@@ -0,0 +1,14 @@
+const Navbar = () => {
+    return (
+        <header>
+            <ul>
+                <li>Home</li>
+                <li>Produtos</li>
+                <li>Como Chegar</li>
+                <li>Fale conosco</li>
+            </ul>
+        </header>
+    )
+}
+
+export default Navbar;
---
+++ src/public/screens/HomeScreen/HomeScreen.jsx
@@ -0,0 +1,11 @@
+import './HomeScreen.css'
+
+const HomeScreen = () => {
+    return (
+        <div id='home-screen' >
+            Seja bem vindo
+        </div>
+    )
+}
+
+export default HomeScreen;
