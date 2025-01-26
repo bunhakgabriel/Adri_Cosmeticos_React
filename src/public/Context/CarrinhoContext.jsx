9e5aca90fcfe878e8f91eba5f4bc71d630dccf37
Gabriel Bunhak <bunhakgabriel@gmail.com>
2025-01-26 17:43:53 -0300

    fix: correções

--- src/public/Context/CarrinhoContext.jsx
+++ src/public/Context/CarrinhoContext.jsx
@@ -1,54 +1,10 @@
 import { createContext, useState } from "react"
 
 export const CarrinhoContext = createContext();
-
-const teste = [
-    {
-      estoque: "10",
-      descricao: "Removedores a base de acetona ...",
-      produto: "ACETONA CINCO",
-      codigo: "7896054719226",
-      colecao: "manicurePedicure",
-      url: "https://firebasestorage.google...",
-      preco: "7.99",
-      quantidade: 1,
-    },
-    // {
-    //   preco: 5.99,
-    //   estoque: 10,
-    //   colecao: "manicurePedicure",
-    //   descricao: "O Removedor de Esmalte com Ace...",
-    //   codigo: "7896902209114",
-    //   produto: "ACETONA FARMAX 100g",
-    //   url: "https://firebasestorage.google...",
-    //   quantidade: 1,
-    // },
-    // {
-    //   descricao: "ADESIVOS EM GEL PARA UNHA MUDE...",
-    //   estoque: "1",
-    //   produto: "ADESIVO GEL P/ UNHA SABRINA SA...",
-    //   preco: "20.00",
-    //   colecao: "manicurePedicure",
-    //   url: "https://firebasestorage.google...",
-    //   codigo: "563101",
-    //   quantidade: 1,
-    // },
-    // {
-    //   estoque: "10",
-    //   colecao: "manicurePedicure",
-    //   codigo: "545801",
-    //   produto: "ADESIVO GEL P/ UNHA SABRINA SA...",
-    //   preco: "20.00",
-    //   descricao: "ADESIVOS EM GEL PARA UNHA MUDE...",
-    //   url: "https://firebasestorage.google...",
-    //   quantidade: 1,
-    // },
-  ];
   
-
 export const CarrinhoProvider = ({children}) => {
 
-    const [carrinho, setCarrinho] = useState(teste);
+    const [carrinho, setCarrinho] = useState([]);
     const [totalProdutos, setTotalProdutos] = useState({quantidade: 0, valor: 0.00});
 
     const adicionarAoCarrinho = (produto) => {
