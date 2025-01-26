8c28b7c5f58a9364d27add51d31c3eb75a975b45
Gabriel Bunhak <bunhakgabriel@gmail.com>
2025-01-25 23:12:26 -0300

    feat: responsivo menu v1

--- src/public/componentes/Navbar/Navbar.jsx
+++ src/public/componentes/Navbar/Navbar.jsx
@@ -1,17 +1,33 @@
 import './Navbar.css'
 import { Link, useLocation } from 'react-router-dom';
+import { IoIosMenu, IoMdClose } from "react-icons/io";
+import { useState } from 'react';
 
 const Navbar = () => {
+    const [mobileMenu, setMobileMenu] = useState(false);
     const path = useLocation().pathname;
 
     return (
         <div id="navbar">
-            {/* <ul>
+            <div className='container-icon' >
+                <IoIosMenu
+                    style={{ display: mobileMenu ? 'none' : 'block' }}
+                    color='white'
+                    size={50}
+                    onClick={() => setMobileMenu(true)}
+                />
+                <IoMdClose 
+                    style={{ display: mobileMenu ? 'block' : 'none' }}
+                    color='white'
+                    size={50}
+                    onClick={() => setMobileMenu(false)} />
+            </div>
+            <ul className={mobileMenu ? 'show-menu' : 'hidden-menu'} >
                 <li>
                     <Link className={`link ${path === '/' ? 'active' : ''}`} to="/">Home</Link>
                 </li>
                 <li>
-                    <Link className={`link ${path === '/produtos' ? 'active' : ''}`}  to="/produtos">Produtos</Link>
+                    <Link className={`link ${path === '/produtos' ? 'active' : ''}`} to="/produtos">Produtos</Link>
                 </li>
                 <li>
                     <Link className='link'>Como Chegar</Link>
@@ -20,9 +36,9 @@ const Navbar = () => {
                     <Link className='link'>Fale conosco</Link>
                 </li>
                 <li>
-                    <Link className={`link ${path === '/carrinho' ? 'active' : ''}`}  to="/carrinho">Carrinho</Link>
+                    <Link className={`link ${path === '/carrinho' ? 'active' : ''}`} to="/carrinho">Carrinho</Link>
                 </li>
-            </ul> */}
+            </ul>
         </div>
     )
 }
