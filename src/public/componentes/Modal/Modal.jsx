f2799abe15b674395b14bbf07dbecd9b212c2bef
Gabriel Bunhak <bunhakgabriel@gmail.com>
2025-01-26 15:15:18 -0300

    feat: responsivo v1 modal

--- src/public/componentes/Modal/Modal.jsx
+++ src/public/componentes/Modal/Modal.jsx
@@ -8,16 +8,12 @@ const Modal = ({ produto, abrirModal, colecao }) => {
                 <div className='produtos-modal'>
                     <img src={produto.url} />
                     <div className='conteudo'>
-                        <p>Código: {produto.codigo}</p>
+                        <p className='codigo' >Código: {produto.codigo}</p>
                         <h1>{produto.produto}</h1>
                         <p>{produto.descricao}</p>
-                        <div className='preco-quantidade' >
+                        <div className='preco' >
                             <h3>R$ {produto.preco}</h3>
-                            {/* <div className='quantidade'>
-                                <IoRemoveOutline style={{ cursor: 'pointer' }} size={30} />
-                                <span>0</span>
-                                <IoAdd style={{ cursor: 'pointer' }} size={30} />
-                            </div> */}
+                            <button className='saiba-mais' >Saiba mais</button>
                         </div>
                     </div>
                 </div>
