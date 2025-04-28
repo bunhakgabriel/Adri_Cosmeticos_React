735c363a7269cedbc2e2210bcd1ca22c0763bae9
bunhakgabriel <bunhakgabriel@gmail.com>
2025-04-28 20:35:02 -0300

    refactor: atualiza url ngrok

--- src/public/hooks/useProduto.jsx
+++ src/public/hooks/useProduto.jsx
@@ -2,8 +2,14 @@ export const useProduto = () => {
 
     const getProdutos = async () => {
         try {
-            const response = await fetch('http://localhost:8080/AdriCosmeticosApi/api/Produtos');
+            const response = await fetch('/AdriCosmeticosApi/api/Produtos', {
+                method: 'GET',
+                headers: {
+                    'ngrok-skip-browser-warning': 'true',  // Adicionando o cabeçalho para ignorar a página de introdução
+                }
+            });
             const data = await response.json();
+            console.log(data)
             return data;
         } catch (e) {
             console.log('ERRO AO OBTER PRODUTOS: ', e)
--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,13 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    proxy: {
+      '/AdriCosmeticosApi': {
+        target: 'https://d8dc-52-167-235-148.ngrok-free.app',  // URL do seu backend
+        changeOrigin: true,               // Faz a troca de origem para evitar problemas com CORS
+        secure: false, 
+      },
+    },
+  },
+});
